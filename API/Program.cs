13a1d637a0d5b5ec0892b55c2a7fbe4b6279582a
N.H.A.V.D.Hangawaththa_it19106670 <it19106670@my.sliit.lk>
2023-01-03 00:18:18 +0530

    minor changes

--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
