2a9b4e8f84c0735ce0f2e6ecd517d6d8e388c297
N.H.A.V.D.Hangawaththa_it19106670 <it19106670@my.sliit.lk>
2023-01-19 01:04:12 +0530

    person details page implementation

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,17 +3,27 @@ import { CommonModule } from '@angular/common';
 import { allIcons, NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
 import { ToastrModule } from 'ngx-toastr';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
     NgxBootstrapIconsModule.pick(allIcons),
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
     }),
+    NgxGalleryModule,
   ],
-  exports: [BsDropdownModule, ToastrModule, NgxBootstrapIconsModule],
+  exports: [
+    BsDropdownModule,
+    ToastrModule,
+    NgxBootstrapIconsModule,
+    TabsModule,
+    NgxGalleryModule,
+  ],
 })
 export class SharedModule {}
