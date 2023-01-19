0bbb450be61e955c34a6d67b01bdac1fe7d9abec
N.H.A.V.D.Hangawaththa_it19106670 <it19106670@my.sliit.lk>
2023-01-19 17:33:47 +0530

    completed User profile implementation

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
+import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css'],
+})
+export class MemberEditComponent implements OnInit {
+  member: Member | undefined;
+  user: User | null = null;
+
+  constructor(
+    private accountService: AccountService,
+    private memberService: MembersService
+  ) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: (user) => (this.user = user),
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    if (!this.user) return;
+    {
+      this.memberService.getMember(this.user.userName).subscribe({
+        next: (member) => (this.member = member),
+      });
+    }
+  }
+}
