28696907838468918cc3bfa23a1cb3b06c13e212
N.H.A.V.D.Hangawaththa_it19106670 <it19106670@my.sliit.lk>
2023-01-28 22:37:09 +0530

    update

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Text.Json;
-using System.Threading.Tasks;
 using API.Errors;
-using Microsoft.Extensions.Options;
 
 namespace API.Middleware
 {
--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using API.Entities;
 using API.Interfaces;
 using Microsoft.IdentityModel.Tokens;
@@ -21,23 +16,24 @@ namespace API.Services
         }
         public string CreateToken(AppUser user)
         {
-           var claims = new List<Claim>{
+            var claims = new List<Claim>{
             new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
            };
 
-           var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
-           var tokenDescriptor = new SecurityTokenDescriptor{
-            Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.Now.AddDays(7),
-            SigningCredentials = creds
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
 
-           };
+            };
 
-           var tokenHandler= new JwtSecurityTokenHandler();
+            var tokenHandler = new JwtSecurityTokenHandler();
 
-           var token = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.CreateToken(tokenDescriptor);
 
-           return tokenHandler.WriteToken(token); 
+            return tokenHandler.WriteToken(token);
 
         }
     }
